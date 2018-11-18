a986a9f5d6f8efedb9344cdfe241b650efbff465
Hristo Gatsinski <gatsinski@gmail.com>
2018-11-18 20:37:50 +0200

    Add `Question` model

---
+++ src/main/java/tuo/model/Question.java
@@ -0,0 +1,39 @@
+package tuo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "questions")
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 254)
+    @Column(unique = true)
+    private String text;
+
+    public Question(Long id, @NotNull @Size(max = 254) String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
