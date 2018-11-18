d145a2c963d1e66939cb79d906d44aaa8e73f954
Hristo Gatsinski <gatsinski@gmail.com>
2018-11-18 20:42:16 +0200

    Add `Answer` model

---
+++ src/main/java/tuo/model/Answer.java
@@ -0,0 +1,55 @@
+package tuo.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "answers")
+public class Answer extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 254)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "question_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Question question;
+
+    public Answer(Long id, @NotNull @Size(max = 254) String text, Question question) {
+        this.id = id;
+        this.text = text;
+        this.question = question;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
