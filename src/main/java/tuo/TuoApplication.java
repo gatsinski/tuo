3f8ece11eb84559da8d2c340480e7ea859dc5c32
Hristo Gatsinski <gatsinski@gmail.com>
2018-11-18 20:22:12 +0200

    Add `Spring` project files

---
+++ src/main/java/tuo/TuoApplication.java
@@ -0,0 +1,12 @@
+package tuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuoApplication.class, args);
+	}
+}
