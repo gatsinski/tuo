cea34ba880a94aa1ffc6943632a9ac827720dd7d
Hristo Gatsinski <gatsinski@gmail.com>
2019-01-02 21:33:57 +0200

    Add `findByText` to `QuestionRepository`

--- src/main/java/tuo/repository/QuestionRepository.java
+++ src/main/java/tuo/repository/QuestionRepository.java
@@ -1,10 +1,14 @@
 package tuo.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tuo.model.Question;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+    Optional<Question> findByText(String text);
 }
