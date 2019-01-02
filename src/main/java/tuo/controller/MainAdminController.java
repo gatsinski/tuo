6f96678eb0c2eda9d5cdf8409cd6c19a5cbcd54c
Hristo Gatsinski <gatsinski@gmail.com>
2019-01-02 21:39:55 +0200

    Add `MainAdminController`

---
+++ src/main/java/tuo/controller/MainAdminController.java
@@ -0,0 +1,15 @@
+package tuo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class MainAdminController {
+
+    @GetMapping
+    public String redirectToQuestions() {
+        return "redirect:/admin/questions";
+    }
+}
