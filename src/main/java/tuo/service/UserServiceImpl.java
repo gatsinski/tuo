95abcf24114ed40088704dee17ad00e8967b30f3
Hristo Gatsinski <gatsinski@gmail.com>
2019-01-02 21:17:37 +0200

    Add `UserServiceImpl`

---
+++ src/main/java/tuo/service/UserServiceImpl.java
@@ -0,0 +1,60 @@
+package tuo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import tuo.dto.UserRegistrationDto;
+import tuo.model.Role;
+import tuo.model.User;
+import tuo.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if (!optionalUser.isPresent()){
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        User user = optionalUser.get();
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+            user.getPassword(),
+            mapRolesToAuthorities(user.getRoles()));
+    }
+
+    public Optional<User> findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(UserRegistrationDto registration){
+        User user = new User();
+        user.setFirstName(registration.getFirstName());
+        user.setLastName(registration.getLastName());
+        user.setEmail(registration.getEmail());
+        user.setPassword(passwordEncoder.encode(registration.getPassword()));
+        user.setRoles(Arrays.asList(new Role("ROLE_USER")));
+        return userRepository.save(user);
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+        return roles.stream()
+            .map(role -> new SimpleGrantedAuthority(role.getName()))
+            .collect(Collectors.toList());
+    }
+}
